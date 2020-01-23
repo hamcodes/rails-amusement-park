f7266aa49ac879f93c54265d49a811a4651b9f3c
github username <github email address>
2020-01-23 09:57:25 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :require_login, :admin_only
 
   def current_user
-    # The session[:user_id] has been set
+ 
     if session[:user_id].present?
       user = User.find_by(:id => session[:user_id])
     end
